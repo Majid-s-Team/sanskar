e9a45b92069c7976c23d6f55393317230b8c619c
arifali-web <arifgopang235@gmail.com>
2025-07-07 19:22:50 +0500

    updated

--- src/app/Forms/RegistrationForm.tsx
+++ src/app/Forms/RegistrationForm.tsx
@@ -5,18 +5,51 @@ import HomeLayout from "../../component/shared/HomeLayout";
 import { registrationForm, step4 } from "../../config";
 import { FeildType } from "../../types";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { ActivityList } from "../../component/partial/ActivityList";
 
 function RegistrationForm() {
+  const [fatherActivity, setFatherActivity] = useState<string[]>([]);
+  const [motherActivity, setMotherActivity] = useState<string[]>([]);
+  const [form] = Form.useForm();
   const navigate = useNavigate();
 
   const onFinish = () => {
     navigate(-1);
   };
+
+  const onUnSelect = (item: string, isFather: boolean) => {
+    const updatedActivities = isFather
+      ? fatherActivity.filter((i) => i !== item)
+      : motherActivity.filter((i) => i !== item);
+    if (isFather) {
+      setFatherActivity(updatedActivities);
+      form.setFieldValue("father_activities", updatedActivities);
+    } else {
+      setMotherActivity(updatedActivities);
+      form.setFieldValue("mother_activities", updatedActivities);
+    }
+  };
+
+  const handleActivityChange = (val: string[], name: string) => {
+    if (name === "father_activities") {
+      setFatherActivity(val);
+      form.setFieldValue(name, val);
+    } else if (name === "mother_activities") {
+      setMotherActivity(val);
+      form.setFieldValue(name, val);
+    }
+  };
   return (
     <HomeLayout>
       <div className="bg-white xl:px-40 lg:px-20  p-5 lg:py-20 rounded-[24.59px] flex flex-col justify-center">
         <p className="text-[30px] text-center semibold">Re-Registration Form</p>
-        <Form onFinish={onFinish} layout="vertical" className="mt-5 ">
+        <Form
+          form={form}
+          onFinish={onFinish}
+          layout="vertical"
+          className="mt-5 "
+        >
           <div className="flex flex-col items-center my-5">
             <Avatar size={120} src="/images/teacher.png" />
             <p className="text-[28px] bold">Child 1</p>
@@ -36,7 +69,7 @@ function RegistrationForm() {
                 );
               })}
             </div>
-            <div>
+            <div className="space-y-5">
               {step4.map((item: FeildType) => {
                 return (
                   <Form.Item
@@ -45,10 +78,27 @@ function RegistrationForm() {
                     name={item.name}
                     rules={item.rules}
                   >
-                    <BaseInput {...item} />
+                    <BaseInput
+                      {...item}
+                      onChange={(val: string[]) =>
+                        handleActivityChange(val, item.name)
+                      }
+                    />
                   </Form.Item>
                 );
               })}
+              <ActivityList
+                activityList={motherActivity}
+                onUnSelect={(item) => onUnSelect(item, false)}
+                title="Mother Activities"
+              />
+
+              {/* Father Activities */}
+              <ActivityList
+                activityList={fatherActivity}
+                onUnSelect={(item) => onUnSelect(item, true)}
+                title="Father Activities"
+              />
             </div>
           </div>
           <div className="flex justify-center w-[100%] mt-10">
