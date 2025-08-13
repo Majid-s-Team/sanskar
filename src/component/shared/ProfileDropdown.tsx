b6a76c65f9128ef5d3e3f0c02eb68d4cb78db635
arifali-web <arifgopang235@gmail.com>
2025-08-13 13:05:54 +0500

    fix build issue

--- src/component/shared/ProfileDropdown.tsx
+++ src/component/shared/ProfileDropdown.tsx
@@ -6,6 +6,8 @@ import { getStorageData } from "../../helper";
 function ProfileDropdown() {
   const { user } = useAuth();
   const role = getStorageData("role");
+  console.log(user, "user");
+
   // Media query for responsive layout: max-width 768px (mobile)
   // const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
@@ -15,7 +17,7 @@ function ProfileDropdown() {
         <Avatar size={50} src={"/icons/notification.png"} />
       </Link>
       <Link to={`${role === "parent" ? "/parents-profile" : "/settings"}`}>
-        <Avatar size={50} src={user?.image_url || "/images/user.png"} />
+        <Avatar size={50} src={"/images/user.png"} />
       </Link>
     </div>
   );
