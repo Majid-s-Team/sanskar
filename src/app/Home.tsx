4a27315ecf89efa0aad08cfabf5e2ddbcfd9c19a
arifali-web <arifgopang235@gmail.com>
2025-08-14 17:40:35 +0500

    text fix width

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -107,7 +107,7 @@ const Home = () => {
                     />
                     <div>
                       <p className="text-[12px] regular">Student Name</p>
-                      <p className="text-[20px] regular capitalize truncate">
+                      <p className="text-[20px] regular capitalize truncate w-[300px]">
                         {activeStudent?.first_name +
                           " " +
                           activeStudent?.last_name}
